cb434e4057efdb7f39281413470b7d9cd5e0c7f5
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-01-31 19:26:24 -0300

    Carpeta ViewModels con su clase

---
+++ GestionTickets/ViewModels/TicketViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GestionTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionTickets.ViewModels
+{
+    //El view model lo usamos cuando necesitamos ver mas de un modelo en la vista
+    //en este view model agrupamos todo lo que necesitamos ver en la vista Nuevo,
+    public class TicketViewModel
+    {
+        public Ticket Ticket { get; set; }
+        public List<Responsable> Responsables { get; set; }
+        public List<Usuario> Usuarios { get; set; }
+        public List<Estado> Estados { get; set; }
+    }
+}
